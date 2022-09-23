464df82aabf599e21f351183467859245d912dd6
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-09-23 07:18:47 +0500

    Add classes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-classes-per-file */
+
 const books = document.querySelector('#books');
 const titleInput = document.querySelector('#title-input');
 const authorInput = document.querySelector('#author-input');
@@ -11,46 +13,52 @@ class Book {
   }
 }
 
-let bookList = [];
+class Methods {
+  constructor() {
+    this.bookList = [];
+  }
 
-function removeBookFromUI(book) {
-  bookList = bookList.filter((item) => {
-    if (book === item) {
-      return false;
-    } return true;
-  });
-  localStorage.setItem('bookArray', JSON.stringify(bookList));
+  removeBookFromUI = (book) => {
+    this.bookList = this.bookList.filter((item) => {
+      if (book === item) {
+        return false;
+      } return true;
+    });
+    localStorage.setItem('bookArray', JSON.stringify(this.bookList));
+  };
+
+  addBookToUI = (book) => {
+    const string = `
+    <div class="book-row">
+      <p>"${book.title}" by ${book.author}</p>
+      <button type="submit" id="remove-btn">Remove</button>
+    </div>
+    `;
+    const bookElement = parser.parseFromString(string, 'text/html').body.firstChild;
+    const removeBtn = bookElement.querySelector('#remove-btn');
+    removeBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.removeBookFromUI(book);
+      bookElement.remove(book);
+    });
+    books.append(bookElement);
+  }
+
+  storeBook = (newBook) => {
+    this.bookList.push(newBook);
+    localStorage.setItem('bookArray', JSON.stringify(this.bookList));
+  };
 }
 
-function addBookToUI(book) {
-  const string = `
-  <div class="book-row">
-    <p>"${book.title}" by ${book.author}</p>
-    <button type="submit" id="remove-btn">Remove</button>
-  </div>
-  `;
-  const bookElement = parser.parseFromString(string, 'text/html').body.firstChild;
-  const removeBtn = bookElement.querySelector('#remove-btn');
-  removeBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    removeBookFromUI(book);
-    bookElement.remove(book);
-  });
-  books.append(bookElement);
-}
-
-function storeBook(newBook) {
-  bookList.push(newBook);
-  localStorage.setItem('bookArray', JSON.stringify(bookList));
-}
+const method = new Methods();
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const newBook = new Book(titleInput.value, authorInput.value);
   titleInput.value = '';
   authorInput.value = '';
-  storeBook(newBook);
-  addBookToUI(newBook);
+  method.storeBook(newBook);
+  method.addBookToUI(newBook);
 });
 
 const booksFromLocalStorage = localStorage.getItem('bookArray');
@@ -58,7 +66,7 @@ if (booksFromLocalStorage !== null) {
   const bookObjectList = JSON.parse(booksFromLocalStorage);
   bookObjectList.forEach((bookObject) => {
     const newBookObject = new Book(bookObject.title, bookObject.author);
-    bookList.push(newBookObject);
+    method.bookList.push(newBookObject);
   });
 }
-bookList.forEach((book) => addBookToUI(book));
+method.bookList.forEach((book) => method.addBookToUI(book));
